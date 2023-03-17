c6d54db1f08def2a12b486e93e8cc4056956527e
wpao <wpaozan@gmail.com>
2023-03-17 18:15:44 +0800

    tes v0.0.2

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
+const { rejects } = require("assert");
 const express = require("express");
+const tes1 = './datas/tes1.json'
+const fs = require('fs');
+const { resolve } = require("path");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res, next) => res.send("Hello Tuan"));
+const test = (props) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(props, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            // mengubah data menjadi objek JavaScript
+            const jsonData = JSON.parse(data);
+            // console.log(jsonData);
+            // res.send(jsonData)
+            resolve(jsonData)
+        })
+    })
+}
+
+app.get("/", (req, res, next) => {
+    res.send("Hello Tuan")
+});
+
+app.get("/tes1", async (req, res, next) => {
+    res.send(await test(tes1))
+});
+
 app.get('/paozan', (req, res) => {
     res.json({
         name: "paozan",
         hody: "Game, conding, Al-Qur'an"
     })
+})
+
+app.get('/tes1', (req, res) => {
+    res.json()
 })
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
